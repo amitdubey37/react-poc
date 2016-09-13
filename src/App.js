848afddcc6a03beab869a6fe99d147405b580a3f
Amit Dubey <amit.dubey@tothenew.com>
2016-09-13 14:34:26 +0530

    add item, increment, decrement and delete item feature added

--- src/App.js
+++ src/App.js
@@ -5,37 +5,51 @@ class App extends Component{
   constructor(props){
     super(props);
     this.state = {
-      'users': [
-        {
-          'name':'Amit',
-          age:25,
-          profession:'Engineer',
-          status:'Active'
-        },
-        {
-          name:'Sachin',
-          age:30,
-          profession:'Tech-Lead',
-          status:'Active'
-        },
-        {
-          name:'Ajay',
-          age:25,
-          profession:'SSE',
-          status:'Active'
-        },
-        {
-          name:'John',
-          age:25,
-          profession:'Manager',
-          status:'Active'
-        },
-      ]
+      'list': [{
+        title:'mango',
+        qty:1,
+        price:30
+      }],
+      item:''
+
     }
   }
-  deleteUser(user){
-    console.log(user);
+
+  handleChange(e){
+    var inputValue = e.target.value
+    this.setState({
+      item:inputValue
+    })
   }
+
+  deleteItem(item){
+    var newState = []
+    this.state.list.map(function(it){
+      if(item.title===it.title){
+
+      }
+      else{
+        newState.push(it)
+      }
+    })
+    this.setState({list:newState})
+    console.log(item);
+  }
+
+  handleSubmit(){
+    var newState =[]
+    this.state.list.map(function(item){
+      newState.push(item)
+    })
+    var title = this.state.item.split('-')[0]
+    var price = this.state.item.split('-')[1]
+    newState.push({title:title,qty:1,price:price})
+    this.setState({
+      item:'',
+      list:newState
+    })
+  }
+
   handleClick(user){
     user.status==='Active'?(user.status='Disabled'):(user.status='Active')
     var newState =[]
@@ -52,41 +66,94 @@ class App extends Component{
       users:newState
     })
   }
+  incrementQty(item){
+    var newList = this.state.list
+    console.log(item);
+    var newState = []
+    item.qty+=1
+    this.state.list.map(function(i){
+      if(item.title === i.title){
+        newState.push(item)
+      }
+      else{
+        newState.push(i)
+      }
+    })
+    newList.push(item)
+    this.setState({list:newState})
+  }
+
+  decrementQty(item){
+    if(item.qty>1){
+      var newList = this.state.list
+      console.log(item);
+      var newState = []
+      item.qty-=1
+      this.state.list.map(function(i){
+        if(item.title === i.title){
+          newState.push(item)
+        }
+        else{
+          newState.push(i)
+        }
+      })
+      newList.push(item)
+      this.setState({list:newState})
+    }
+
+  }
   render(){
     var _this = this;
     var hStyle = {
       width:'200px'
     }
-    var users = this.state.users.map(function(user,index){
+    var users = this.state.list.map(function(item,index){
       return (
-        <tr key={index} className={user.status}>
-          <td>{user.name}</td>
-          <td>{user.age}</td>
-          <td>{user.profession}</td>
-          <td>{user.status}</td>
-          <td><a className='btn btn-success' onClick={_this.handleClick.bind(_this,user)}>{user.status==='Active'?'Disable':'Enable'}</a>
-            <a className='btn btn-danger' onClick={_this.deleteUser.bind(_this,user)}>delete</a>
-          </td>
-        </tr>
+          <div className="row list-group-item list-group-item-action list-group-item-success" key={index}>
+            <div className="col-xs-3">
+              <span>
+                {item.title}
+              </span>
+            </div>
+            <div className="col-xs-1">
+              {item.qty}
+            </div>
+
+            <div className="col-xs-3">
+                {item.qty*item.price}
+            </div>
+
+            <div className="col-xs-1" onClick={_this.decrementQty.bind(_this,item)}>
+              <i className="glyphicon glyphicon-minus bt" ></i>
+            </div>
+            <div className="col-xs-1" onClick={_this.incrementQty.bind(_this,item)}>
+                <i className="glyphicon glyphicon-plus bt"></i>
+            </div>
+
+            <div className="col-xs-3">
+              <span id="deleteItem" className='glyphicon glyphicon-remove-circle' onClick={_this.deleteItem.bind(_this,item)}></span>
+            </div>
+          </div>
       )
     })
     return(
       <div className="container">
-      <h2>Basic user list</h2>
-      <table className="table">
-  <thead>
-    <tr>
-      <th>Name</th>
-      <th>Age</th>
-      <th>Profession</th>
-      <th style={hStyle}>Status</th>
-      <th style={hStyle}>Action</th>
-    </tr>
-  </thead>
-  <tbody>
-    {users}
-  </tbody>
-</table>
+      <div className="row">
+        <div className='col-xs-3'></div>
+        <div className="col-xs-6">
+        <h3>Todos</h3>
+        <form action="" className="form-inline" onSubmit={this.handleSubmit.bind(this)}>
+          <div className="form-group">
+            <input type="text" className="form-control" value={this.state.item} placeholder="Enter item" onChange={this.handleChange.bind(this)}/>
+          </div>
+        </form>
+        <div className="list-group">
+          {users}
+        </div>
+        </div>
+        <div className='col-xs-3'></div>
+      </div>
+
       </div>
     )
   }

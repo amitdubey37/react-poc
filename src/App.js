4208f30b7996448f52b5ed91a1ed1060074523eb
Amit Dubey <amit.dubey@tothenew.com>
2016-09-22 10:02:54 +0530

    live search implemented

--- src/App.js
+++ src/App.js
@@ -1,167 +1,44 @@
 import React, { Component } from 'react';
 import CartTotal from './CartTotal'
-// import { Label, DropdownButton, MenuItem, Form } from 'react-bootstrap'
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            'list': [{
-                title: 'mango',
-                qty: 1,
-                price: 30
-            }],
-            item: ''
 
-        }
-    }
-
-    handleChange(e) {
-        var inputValue = e.target.value;
-        this.setState({
-            item: inputValue
-        })
-    }
-
-    deleteItem(item) {
-        var newState = [];
-        this.state.list.map(function (it) {
-            if (item.title === it.title) {
-
-            }
-            else {
-                newState.push(it)
-            }
-        });
-        this.setState({list: newState});
-        console.log(item);
-    }
-
-    handleSubmit(e) {
-        e.preventDefault();
-        var newState = [];
-        this.state.list.map(function (item) {
-            newState.push(item)
-        });
-        var title = this.state.item.split('-')[0];
-        var price = this.state.item.split('-')[1];
-        newState.push({title: title, qty: 1, price: price});
-        this.setState({
-            item: '',
-            list: newState
-        })
-    }
-
-    handleClick(user) {
-        user.status === 'Active' ? (user.status = 'Disabled') : (user.status = 'Active');
-        var newState = [];
-
-        this.state.users.map(function (u) {
-            if (user.name !== u.name) {
-                newState.push(u);
-            }
-            else {
-                newState.push(user)
-            }
-        });
-        this.setState({
-            users: newState
-        })
-    }
-
-    incrementQty(item) {
-        var newList = this.state.list;
-        console.log(item);
-        var newState = [];
-        item.qty += 1;
-        this.state.list.map(function (i) {
-            if (item.title === i.title) {
-                newState.push(item)
-            }
-            else {
-                newState.push(i)
-            }
-        });
-        newList.push(item);
-        this.setState({list: newState})
-    }
-
-    decrementQty(item) {
-        if (item.qty > 1) {
-            var newList = this.state.list;
-            console.log(item);
-            var newState = [];
-            item.qty -= 1;
-            this.state.list.map(function (i) {
-                if (item.title === i.title) {
-                    newState.push(item)
-                }
-                else {
-                    newState.push(i)
-                }
-            });
-            newList.push(item);
-            this.setState({list: newState})
+        constructor(props){
+            super(props)
+            this.state =  { searchString: '' };
+            this.handleChange = this.handleChange.bind(this)
         }
 
-    }
+        handleChange(e){
+            console.log('handle change')
+            this.setState({searchString:e.target.value});
+        }
 
-    render() {
-        var _this = this;
-        var hStyle = {
-            width: '200px'
-        };
-        var items = this.state.list.map(function (item, index) {
+        render(){
+            var libraries = this.props.items , searchString = this.state.searchString.trim().toLowerCase();
+            if(searchString.length > 0){
+
+                libraries = libraries.filter(function(l){
+                    return l.name.toLowerCase().match( searchString );
+                });
+            }
+
             return (
-                <div className="row list-group-item list-group-item-action list-group-item-success" key={index}>
-                    <div className="col-xs-3">
-                        <span>
-                            {item.title}
-                        </span>
-                    </div>
-                    <div className="col-xs-1">
-                        {item.qty}
-                    </div>
+                <div>
+                <input type="text" value={this.state.searchString} onChange={this.handleChange} placeholder="Type here" />
 
-                    <div className="col-xs-3">
-                    {item.qty * item.price}
-                    </div>
+                <ul>
 
-                    <div className="col-xs-1" onClick={_this.decrementQty.bind(_this, item)}>
-                        <i className="glyphicon glyphicon-minus bt" ></i>
-                    </div>
-                    <div className="col-xs-1" onClick={_this.incrementQty.bind(_this, item)}>
-                        <i className="glyphicon glyphicon-plus bt"></i>
-                    </div>
+                        { libraries.map(function(l){
+                            return <li>{l.name} <a href={l.url}>{l.url}</a></li>
+                        }) }
 
-                    <div className="col-xs-3">
-                        <span id="deleteItem" className='glyphicon glyphicon-remove-circle' onClick={_this.deleteItem.bind(_this, item)}></span>
-                    </div>
-                </div>
-            )
-        });
-        return (
-            <div className="container">
-                <div className="row">
-                    <div className='col-xs-3'></div>
-                    <div className="col-xs-6">
-                        <h3>Todos</h3>
-                        <form action="" className="form-inline" onSubmit={this.handleSubmit.bind(this)}>
-                            <div className="form-group">
-                                <input type="text" className="form-control" value={this.state.item} placeholder="Enter item" onChange={this.handleChange.bind(this)}/>
-                            </div>
-                        </form>
-                        <div className="list-group">
-                            {items}
-                        </div>
-                    </div>
-                    <div className='col-xs-3'></div>
-                </div>
-                <CartTotal items={this.state.list}/>
-
+                </ul>
 
             </div>
-        )
-    }
+
+            )
+        }
+
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+var libraries = [
 
+  {name: 'Backbone.js', url: 'http://documentcloud.github.io/backbone/'},
+  {name: 'AngularJS', url: 'https://angularjs.org/'},
+  {name: 'jQuery', url: 'http://jquery.com/'},
+  {name: 'Prototype', url: 'http://www.prototypejs.org/'},
+  {name: 'React', url: 'http://facebook.github.io/react/'},
+  {name: 'Ember', url: 'http://emberjs.com/'},
+  {name: 'Knockout.js', url: 'http://knockoutjs.com/'},
+  {name: 'Dojo', url: 'http://dojotoolkit.org/'},
+  {name: 'Gradle', url: 'http://gradle.org/'},
+  {name: 'Underscore', url: 'http://documentcloud.github.io/underscore/'},
+  {name: 'Lodash', url: 'http://lodash.com/'},
+  {name: 'Moment', url: 'http://momentjs.com/'},
+  {name: 'Express', url: 'http://expressjs.com/'},
+  {name: 'Grails', url: 'http://grails.org/'}
+
+];
 ReactDOM.render(
-  <App />,
+  <App items ={libraries} />,
   document.getElementById('app')
 );

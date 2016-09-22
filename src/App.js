8a296bacc72485cbc32e8f473bb3141587192af5
Amit Dubey <amit.dubey@tothenew.com>
2016-09-22 10:13:56 +0530

    refactored and removed warnigns

--- src/App.js
+++ src/App.js
@@ -1,44 +1,45 @@
 import React, { Component } from 'react';
-import CartTotal from './CartTotal'
 
 class App extends Component {
 
-        constructor(props){
-            super(props)
-            this.state =  { searchString: '' };
-            this.handleChange = this.handleChange.bind(this)
+    constructor(props) {
+        super(props)
+        this.state = {searchString: ''};
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(e) {
+        console.log('handle change')
+        this.setState({searchString: e.target.value});
+    }
+
+    render() {
+        var libraries = this.props.items, searchString = this.state.searchString.trim().toLowerCase();
+        if (searchString.length > 0) {
+
+            libraries = libraries.filter(function (l) {
+                return l.name.toLowerCase().match(searchString);
+            });
         }
 
-        handleChange(e){
-            console.log('handle change')
-            this.setState({searchString:e.target.value});
-        }
-
-        render(){
-            var libraries = this.props.items , searchString = this.state.searchString.trim().toLowerCase();
-            if(searchString.length > 0){
-
-                libraries = libraries.filter(function(l){
-                    return l.name.toLowerCase().match( searchString );
-                });
-            }
-
-            return (
-                <div>
+        return (
+            <div>
                 <input type="text" value={this.state.searchString} onChange={this.handleChange} placeholder="Type here" />
 
                 <ul>
 
-                        { libraries.map(function(l){
-                            return <li>{l.name} <a href={l.url}>{l.url}</a></li>
+                        { libraries.map(function (library, index) {
+                            return <li key={index}>{library.name}
+                                <a href={library.url}>{library.url}</a>
+                            </li>
                         }) }
 
                 </ul>
 
             </div>
 
-            )
-        }
+        )
+    }
 
 }
 export default App;

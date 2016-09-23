ff214d9630ccd8028a2df897877966cfc742fa95
Amit Dubey <amit.dubey@tothenew.com>
2016-09-23 15:14:58 +0530

    refactored Multiple Components Created;

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import CartTotal from './CartTotal'
+import ListItem from './ListItem'
+import AddItemForm from './AddItemForm'
 // import { Label, DropdownButton, MenuItem, Form } from 'react-bootstrap'
 
 class App extends Component {
@@ -13,7 +15,12 @@ class App extends Component {
             }],
             item: ''
 
-        }
+        };
+        this.deleteItem = this.deleteItem.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.incrementQty =  this.incrementQty.bind(this);
+        this.decrementQty = this.decrementQty.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(e) {
@@ -34,22 +41,31 @@ class App extends Component {
             }
         });
         this.setState({list: newState});
-        console.log(item);
     }
 
     handleSubmit(e) {
         e.preventDefault();
-        var newState = [];
-        this.state.list.map(function (item) {
-            newState.push(item)
-        });
+
         var title = this.state.item.split('-')[0];
         var price = this.state.item.split('-')[1];
-        newState.push({title: title, qty: 1, price: price});
-        this.setState({
-            item: '',
-            list: newState
-        })
+        if(isNaN(price)){
+            this.setState({
+                error:'Invalid input'
+            })
+
+        }else{
+            var newState = [];
+            this.state.list.map(function (item) {
+                newState.push(item)
+            });
+            newState.push({title: title, qty: 1, price: price});
+            this.setState({
+                item: '',
+                list: newState,
+                error:''
+            })
+        }
+
     }
 
     handleClick(user) {
@@ -71,7 +87,6 @@ class App extends Component {
 
     incrementQty(item) {
         var newList = this.state.list;
-        console.log(item);
         var newState = [];
         item.qty += 1;
         this.state.list.map(function (i) {
@@ -89,7 +104,6 @@ class App extends Component {
     decrementQty(item) {
         if (item.qty > 1) {
             var newList = this.state.list;
-            console.log(item);
             var newState = [];
             item.qty -= 1;
             this.state.list.map(function (i) {
@@ -107,37 +121,12 @@ class App extends Component {
     }
 
     render() {
-        var _this = this;
         var hStyle = {
             width: '200px'
         };
-        var items = this.state.list.map(function (item, index) {
+        var items = this.state.list.map( (item, index) => {
             return (
-                <div className="row list-group-item list-group-item-action list-group-item-success" key={index}>
-                    <div className="col-xs-3">
-                        <span>
-                            {item.title}
-                        </span>
-                    </div>
-                    <div className="col-xs-1">
-                        {item.qty}
-                    </div>
-
-                    <div className="col-xs-3">
-                    {item.qty * item.price}
-                    </div>
-
-                    <div className="col-xs-1" onClick={_this.decrementQty.bind(_this, item)}>
-                        <i className="glyphicon glyphicon-minus bt" ></i>
-                    </div>
-                    <div className="col-xs-1" onClick={_this.incrementQty.bind(_this, item)}>
-                        <i className="glyphicon glyphicon-plus bt"></i>
-                    </div>
-
-                    <div className="col-xs-3">
-                        <span id="deleteItem" className='glyphicon glyphicon-remove-circle' onClick={_this.deleteItem.bind(_this, item)}></span>
-                    </div>
-                </div>
+                    <ListItem item={item} deleteItem={this.deleteItem} decrementQty={this.decrementQty} incrementQty={this.incrementQty}/>
             )
         });
         return (
@@ -145,12 +134,9 @@ class App extends Component {
                 <div className="row">
                     <div className='col-xs-3'></div>
                     <div className="col-xs-6">
-                        <h3>Todos</h3>
-                        <form action="" className="form-inline" onSubmit={this.handleSubmit.bind(this)}>
-                            <div className="form-group">
-                                <input type="text" className="form-control" value={this.state.item} placeholder="Enter item" onChange={this.handleChange.bind(this)}/>
-                            </div>
-                        </form>
+                        <h3>Listonic</h3>
+                        <AddItemForm item={this.state.item} onChange = {this.handleChange} onSubmit={this.handleSubmit}/>
+                        <p className="error">{this.state.error}</p>
                         <div className="list-group">
                             {items}
                         </div>
@@ -158,10 +144,10 @@ class App extends Component {
                     <div className='col-xs-3'></div>
                 </div>
                 <CartTotal items={this.state.list}/>
-
-
             </div>
         )
     }
 }
+
+
 export default App;
